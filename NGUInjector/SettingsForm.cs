340b341c7555514a1d75891bb434ad53b97c20dd
Karol Lassak <ingwar@ingwar.eu.org>
2021-04-22 21:24:48 +0200

    Mooooar cleaning

--- NGUInjector/SettingsForm.cs
+++ NGUInjector/SettingsForm.cs
@@ -1,110 +1,23 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Globalization;
-using System.Linq;
-using System.Text;
 using System.Windows.Forms;
-//using NGUInjector.AllocationProfiles;
-//using NGUInjector.Managers;
 
 namespace NGUInjector
 {
     public partial class SettingsForm : Form
     {
-        internal readonly Dictionary<int, string> TitanList;
-        internal readonly Dictionary<int, string> ZoneList;
-        internal readonly Dictionary<int, string> CombatModeList;
-        internal readonly Dictionary<int, string> CubePriorityList;
         private bool _initializing = true;
+
         public SettingsForm()
         {
             InitializeComponent();
-
-            CubePriorityList = new Dictionary<int, string>();
-            CombatModeList = new Dictionary<int, string>();
-            ZoneList = new Dictionary<int, string>();
-            TitanList = new Dictionary<int, string>();
-            // Populate our data sources
-            TitanList.Add(0, "None");
-            TitanList.Add(1, "GRB");
-            TitanList.Add(2, "GCT");
-            TitanList.Add(3, "Jake");
-            TitanList.Add(4, "UUG");
-            TitanList.Add(5, "Walderp");
-            TitanList.Add(6, "Beast");
-            TitanList.Add(7, "Greasy Nerd");
-            TitanList.Add(8, "Godmother");
-            TitanList.Add(9, "Exile");
-            TitanList.Add(10, "IT HUNGERS");
-            TitanList.Add(11, "Rock Lobster");
-            TitanList.Add(12, "Amalgamate");
-
-            ZoneList.Add(-1, "Safe Zone: Awakening Site");
-            ZoneList.Add(0, "Tutorial Zone");
-            ZoneList.Add(1, "Sewers");
-            ZoneList.Add(2, "Forest");
-            ZoneList.Add(3, "Cave of Many Things");
-            ZoneList.Add(4, "The Sky");
-            ZoneList.Add(5, "High Security Base");
-            ZoneList.Add(6, "Gordon Ramsay Bolton");
-            ZoneList.Add(7, "Clock Dimension");
-            ZoneList.Add(8, "Grand Corrupted Tree");
-            ZoneList.Add(9, "The 2D Universe");
-            ZoneList.Add(10, "Ancient Battlefield");
-            ZoneList.Add(11, "Jake From Accounting");
-            ZoneList.Add(12, "A Very Strange Place");
-            ZoneList.Add(13, "Mega Lands");
-            ZoneList.Add(14, "UUG THE UNMENTIONABLE");
-            ZoneList.Add(15, "The Beardverse");
-            ZoneList.Add(16, "WALDERP");
-            ZoneList.Add(17, "Badly Drawn World");
-            ZoneList.Add(18, "Boring-Ass Earth");
-            ZoneList.Add(19, "THE BEAST");
-            ZoneList.Add(20, "Chocolate World");
-            ZoneList.Add(21, "The Evilverse");
-            ZoneList.Add(22, "Pretty Pink Princess Land");
-            ZoneList.Add(23, "GREASY NERD");
-            ZoneList.Add(24, "Meta Land");
-            ZoneList.Add(25, "Interdimensional Party");
-            ZoneList.Add(26, "THE GODMOTHER");
-            ZoneList.Add(27, "Typo Zonw");
-            ZoneList.Add(28, "The Fad-Lands");
-            ZoneList.Add(29, "JRPGVille");
-            ZoneList.Add(30, "THE EXILE");
-            ZoneList.Add(31, "The Rad-lands");
-            ZoneList.Add(32, "Back To School");
-            ZoneList.Add(33, "The West World");
-            ZoneList.Add(34, "IT HUNGERS");
-            ZoneList.Add(35, "The Breadverse");
-            ZoneList.Add(36, "That 70's Zone");
-            ZoneList.Add(37, "The Halloweenies");
-            ZoneList.Add(38, "ROCK LOBSTER");
-            ZoneList.Add(39, "Construction Zone");
-            ZoneList.Add(40, "DUCK DUCK ZONE");
-            ZoneList.Add(41, "The Nether Regions");
-            ZoneList.Add(42, "AMALGAMATE");
-            ZoneList.Add(1000, "ITOPOD");
-
-            CombatModeList.Add(0, "Manual");
-            CombatModeList.Add(1, "Idle");
-
-            CubePriorityList.Add(0, "None");
-            CubePriorityList.Add(1, "Balanced");
-            CubePriorityList.Add(2, "Power");
-            CubePriorityList.Add(3, "Toughness");
-
-            //Remove ITOPOD for non combat zones
-            ZoneList.Remove(1000);
-
             VersionLabel.Text = $"Version: {Main.Version}";
         }
         
         internal void UpdateFromSettings(SavedSettings newSettings)
         {
             _initializing = true;
+
+            MasterEnable.Checked = newSettings.GlobalEnabled;
             AutoDailySpin.Checked = newSettings.AutoSpin;
             AutoITOPOD.Checked = newSettings.AutoQuestITOPOD;
             AutoMoneyPit.Checked = newSettings.AutoMoneyPit;
