fc843b73f9fb28ba9b11c0b94dd152c3be571d00
Karol Lassak <ingwar@ingwar.eu.org>
2021-04-26 15:04:35 +0200

    Add missing file + part of prority queue

---
+++ MaterialState.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace NGUIndustriesInjector
+{
+    class MaterialState
+    {
+        private const int WANT_SECONDS = 3600 * 10;
+
+        private static List<MaterialState> allMaterials;
+        private static Player player;
+        private static TrackerController tracker;
+
+        private readonly MaterialData materialData;
+        private readonly BuildingProperties properties;
+
+        internal BuildingType BuildingType { get; }
+        internal int BuildingId { get; set; }
+        internal int BuildNumber { get; set; }
+        internal int BuildPercent { get; set; }
+
+        private Func<MaterialState, bool> DeleteFunction;
+
+        internal MaterialState(BuildingType buildingType)
+        {
+            this.BuildingType = buildingType;
+            this.BuildingId = (int)buildingType;
+            this.materialData = player.materials.materials[BuildingId];
+            this.properties = tracker.buildingProperties.properties[BuildingId];
+        }
+
+        internal static void SetState(List<MaterialState> allMaterials, Player player)
+        {
+            MaterialState.allMaterials = allMaterials;
+            MaterialState.player = player;
+            tracker = player.factoryController.tracker;
+        }
+
+        internal bool Unlocked
+        {
+            get => materialData.unlocked;
+        }
+
+        internal long Amount
+        {
+            get => materialData.amount;
+        }
+
+        internal double Gain
+        {
+            get => tracker.theoreticalGainLoss[BuildingId];
+        }
+
+        internal double BaseTime
+        {
+            get => properties.baseTime;
+        }
+
+        internal double MinTime
+        {
+            get => properties.minTime;
+        }
+
+        internal double PerSecond
+        {
+            get => materialData.largestProduction / properties.baseTime;
+        }
+
+        internal int BuildingCount
+        {
+            get => tracker.buildingCounts[BuildingId];
+        }
+
+        internal string Name
+        {
+            get => properties.name;
+        }
+
+        internal bool IsJuice
+        {
+            get => Name.Contains("Juice");
+        }
+
+        internal double ShouldHave
+        {
+            get
+            {
+                //var prio = Main.Settings.PriorityBuildings.Find(b => (int)b.x == BuildingId);
+                //if (prio != null)
+                 //   return prio.y;
+
+                return PerSecond* WANT_SECONDS *(IsJuice ? 100 : 1);
+            }
+        }
+
+        internal double HavePercent
+        {
+            get
+            {
+                if (ShouldHave > 0)
+                    return Amount / ShouldHave * 100;
+                return 0;
+            }
+        }
+
+        internal List<BuildingType> ToDelete
+        {
+            get
+            {
+                var data = from entry in allMaterials where entry.BuildingCount > 0 && DeleteFunction.Invoke(entry) select entry.BuildingType;
+                return data.ToList();
+            }
+        }
+
+        internal void CalculateState()
+        {
+            BuildNumber = 0;
+            BuildPercent = 0;
+            DeleteFunction = other => other.HavePercent > BuildPercent && other.BuildNumber == 0;
+
+            Main.Log($"{this}: Calculating");
+
+            if (!Unlocked)
+                return;
+
+            if (PerSecond <= 0)
+                return;
+
+            if (Main.Settings.FactoryDontStarve && HavePercent < 10 && Gain < 0)
+            {
+                BuildNumber = -(int)(Gain / PerSecond / Math.Max(1, HavePercent));
+                BuildNumber = Math.Max(1, Math.Min(BuildNumber, 30));
+                BuildPercent = 5;
+                DeleteFunction = other => other.HavePercent > BuildPercent && other.Gain > 0;
+                Main.Log($"{this}: Adding DONT STARVE to SET < 10% and gain < 0 to build {BuildNumber}");
+                return;
+            }
+
+            var prio = Main.Settings.PriorityBuildings.Find(b => (int)b.x == BuildingId);
+            if (prio != null)
+            {
+                var needed = prio.y;
+                Main.Log($"{this}: Is on prio list with needed {needed}");
+                if (needed > Amount)
+                {
+                    BuildNumber = 1;
+                    BuildPercent = 100;
+                    Main.Log($"{this}: Adding PRIO MODE build neede {needed} > have {Amount}");
+                    return;
+                }
+            }
+
+            if (Main.Settings.FactoryBuildStandard)
+            {
+                if (HavePercent < 10 && BuildingCount < 3)
+                {
+                    BuildNumber = 1;
+                    BuildPercent = 200;
+                    Main.Log($"{this}: Adding BUILD STANDARD to SET < 10% and less than 3 instances");
+                    return;
+                }
+                else if (HavePercent < 1)
+                {
+                    BuildNumber = 1;
+                    BuildPercent = 200;
+                    Main.Log($"{this}: Adding BUILD STANDARD to SET < 1%");
+                    return;
+                }
+                else if (HavePercent < 100 && Gain <= 0)
+                {
+                    BuildNumber = 1;
+                    BuildPercent = 200;
+                    Main.Log($"{this}: Adding BUILD STANDARD to SET < 100% and gain <= 0");
+                    return;
+                }
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"[{BuildingId} {Name}: {ShouldHave} % {HavePercent} build {BuildNumber} on % {BuildPercent}]";
+        }
+    }
+}
--- SettingsForm.Designer.cs
+++ SettingsForm.Designer.cs
@@ -45,6 +45,13 @@
             this.tabPage2 = new System.Windows.Forms.TabPage();
             this.FactoryBuildStandard = new System.Windows.Forms.CheckBox();
             this.FactoryDontStarve = new System.Windows.Forms.CheckBox();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.Name = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Number = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.button1 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.maskedTextBox1 = new System.Windows.Forms.MaskedTextBox();
+            this.button2 = new System.Windows.Forms.Button();
             this.flowLayoutPanel1.SuspendLayout();
             this.tabControl1.SuspendLayout();
             this.tabPage1.SuspendLayout();
@@ -203,6 +210,11 @@
             // 
             // tabPage2
             // 
+            this.tabPage2.Controls.Add(this.button2);
+            this.tabPage2.Controls.Add(this.maskedTextBox1);
+            this.tabPage2.Controls.Add(this.comboBox1);
+            this.tabPage2.Controls.Add(this.button1);
+            this.tabPage2.Controls.Add(this.listView1);
             this.tabPage2.Controls.Add(this.FactoryBuildStandard);
             this.tabPage2.Controls.Add(this.FactoryDontStarve);
             this.tabPage2.Location = new System.Drawing.Point(4, 22);
@@ -234,6 +246,62 @@
             this.FactoryDontStarve.Text = "Dont allow starving of factories (never see red color again)";
             this.FactoryDontStarve.UseVisualStyleBackColor = true;
             this.FactoryDontStarve.CheckedChanged += new System.EventHandler(this.FactoryDontStarve_CheckedChanged);
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.Name,
+            this.Number});
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(7, 64);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(194, 153);
+            this.listView1.TabIndex = 2;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.List;
+            // 
+            // Name
+            // 
+            this.Name.Text = "Material Name";
+            // 
+            // Number
+            // 
+            this.Number.Text = "Number wanted";
+            // 
+            // button1
+            // 
+            this.button1.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.button1.Location = new System.Drawing.Point(489, 252);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(76, 20);
+            this.button1.TabIndex = 13;
+            this.button1.Text = "Save";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Visible = false;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(208, 196);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 14;
+            // 
+            // maskedTextBox1
+            // 
+            this.maskedTextBox1.Location = new System.Drawing.Point(346, 196);
+            this.maskedTextBox1.Name = "maskedTextBox1";
+            this.maskedTextBox1.Size = new System.Drawing.Size(100, 20);
+            this.maskedTextBox1.TabIndex = 15;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(489, 194);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 16;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
             // 
             // SettingsForm
             // 
@@ -271,5 +339,12 @@
         private System.Windows.Forms.TabPage tabPage2;
         private System.Windows.Forms.CheckBox FactoryDontStarve;
         private System.Windows.Forms.CheckBox FactoryBuildStandard;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.MaskedTextBox maskedTextBox1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader Name;
+        private System.Windows.Forms.ColumnHeader Number;
     }
 }
