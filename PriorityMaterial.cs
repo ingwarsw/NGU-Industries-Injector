c4563cc7e4a3f25575169621fc09363fd1fac3b5
Karol Lassak <ingwar@ingwar.eu.org>
2021-04-30 12:43:05 +0200

    Add missing class

---
+++ PriorityMaterial.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel;
+
+namespace NGUIndustriesInjector
+{
+    public class PriorityMaterial
+    {
+        public BuildingType Type { get; set; }
+        public long Want { get; set; }
+    }
+}
